c35497353ba0e0e48ecfaeef22e393c623ce12e2
ky-hiverge <kante@hiverge.ai>
2025-07-18 17:36:15 +0100

    update readme.md
    Signed-off-by: ky-hiverge <kante@hiverge.ai>

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# hive-cli
-The CLi for Hive.
+# Hive CLI
+
+The CLI for Hive.
+
+## How to Setup
+
+Run `source start.sh` to setup the environment.
+
+Run `deactivate` to exit the environment.
+
+## How to Run
+
+Support commands:
+```
+init                Initialize a repository
+create              Create resources
+delete              Delete resources
+```
+
+See `hive -h` for more details.
---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script to setup the environment for running experiments with the Hive.
+
+# check whether uv is installed
+if ! command -v uv &> /dev/null; then
+  echo "Start to install uv..."
+
+  # Install via brew by default.
+  if command -v brew &> /dev/null; then
+    brew install uv
+  else
+    # If brew not found, use the official install script.
+    curl -Ls https://astral.sh/uv/install.sh | sh
+  fi
+
+  echo "uv installed successfully."
+fi
+
+uv venv --python 3.12
+source .venv/bin/activate
+uv pip install -e .
